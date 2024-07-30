a501110c60154082a249957a30d7d6944cc3a9c1
Maahmoud-Elsayed <mahmoud.elsayed.elbadawy@gmail.com>
2024-07-30 03:39:18 +0300

    Initial commit

---
+++ components/project.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import { useRef } from "react";
+import { projectsData } from "@/lib/data";
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Link from "next/link";
+
+type ProjectProps = (typeof projectsData)[number];
+
+export default function Project({
+  title,
+  description,
+  tags,
+  imageUrl,
+  sourceUrl,
+  liveUrl,
+}: ProjectProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.26 1"],
+  });
+  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
+  return (
+    <motion.div
+      ref={ref}
+      style={{
+        scale: scaleProgess,
+        opacity: opacityProgess,
+      }}
+      className="group mb-3 sm:mb-8 last:mb-0"
+    >
+      <section className="bg-gray-100  max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative  hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="py-4 px-5 sm:pl-10 gap-4 sm:pr-2 sm:py-6 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
+          <h3 className="text-2xl font-semibold">{title}</h3>
+          <p className=" text-sm text-gray-700 dark:text-white/70">
+            {description}
+          </p>
+          <ul className="flex flex-wrap  gap-2 sm:mt-auto">
+            {tags.map((tag, index) => (
+              <li
+                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                key={index}
+              >
+                {tag}
+              </li>
+            ))}
+          </ul>
+
+          <div className="flex gap-4 ">
+            <Link
+              href={liveUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group text-sm bg-gray-900 cursor-pointer text-white px-4 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+            >
+              Preview
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                />
+              </svg>
+            </Link>
+            <Link
+              href={sourceUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group whitespace-nowrap text-sm bg-gray-900 cursor-pointer text-white px-4 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+            >
+              Source code
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+              </svg>
+            </Link>
+          </div>
+        </div>
+
+        <Image
+          src={imageUrl}
+          alt="Project I worked on"
+          quality={95}
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+        transition 
+        group-hover:scale-[1.04]
+        group-hover:-translate-x-3
+        group-hover:translate-y-3
+        group-hover:-rotate-2
+
+        group-even:group-hover:translate-x-3
+        group-even:group-hover:translate-y-3
+        group-even:group-hover:rotate-2
+
+        group-even:right-[initial] group-even:-left-40"
+        />
+      </section>
+    </motion.div>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,108 @@
+import yummy from "@/public/yummy.png";
+import fastway from "@/public/fastway.png";
+import adminPanel from "@/public/admin-panel.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Yummy",
+    description:
+      "An online food ordering app built with the T3 Stack, featuring authentication and payment integration. It includes a robust admin panel for managing users, orders, categories, and products.",
+    tags: [
+      "Next.js",
+      "Typescript",
+      "TRPC",
+      "Tailwind",
+      "NextAuth",
+      "Prisma",
+      "React Query",
+      "React Hook Form",
+      "Zod",
+      "Neon db",
+      "Stripe",
+      "Zustand",
+      "Framer Motion",
+      "Chadcn UI",
+      "Responsive Design",
+    ],
+    imageUrl: yummy,
+    sourceUrl: "https://github.com/Maahmoud-Elsayed/Yummy-online-food",
+    liveUrl: "https://yummyfoods.vercel.app",
+  },
+  {
+    title: "Fastway",
+    description:
+      "An eCommerce platform designed for seamless shopping experiences. Built with modern web technologies, it offers secure user authentication, diverse product listings, and a user-friendly interface for managing orders, inventory, and customer interactions.",
+    tags: [
+      "React.js",
+      "Redux Toolkit",
+      "Formik",
+      "Tailwind",
+      "Yup",
+      "Firebase",
+      "Responsive Design",
+    ],
+    imageUrl: fastway,
+    sourceUrl: "https://github.com/Maahmoud-Elsayed/Fastway-eCommerce",
+    liveUrl: "https://fast-way-913c7.web.app",
+  },
+  {
+    title: "Admin-Dashboard",
+    description: "Admin Panel Dashboard with dark and light mode",
+    tags: [
+      "React.js",
+      "Material UI",
+      "Tailwind",
+      "React hook form",
+      "typescript",
+      "zod",
+      "charts",
+      "responsive design",
+    ],
+    imageUrl: adminPanel,
+    sourceUrl: "https://github.com/Maahmoud-Elsayed/Admin-Dashboard",
+    liveUrl: "https://admin-x-panel.vercel.app",
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React.js",
+  "Next.js",
+  "Git",
+  "GitHub",
+  "Tailwind",
+  "Material UI",
+  "SASS",
+  "ES6",
+  "Responsive Design",
+  "Redux",
+  "React Query",
+  "Framer Motion",
+  "Jest",
+] as const;
